4cc8251891ae7dacda9647feb8d926baddce0fb2
Stuart Carruthers <stu@ironiclensflare.com>
2020-06-07 11:51:44 +0100

    Will this work?

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/ironiclensflare/printer
 
 go 1.14
 
-require github.com/aws/aws-sdk-go v1.31.12
+require (
+	github.com/aws/aws-sdk-go v1.31.12
+	github.com/desertbit/fillpdf v0.0.0-20150721083828-cece5eb91157 // indirect
+)
--- printer.go
+++ printer.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/sqs"
+	"github.com/desertbit/fillpdf"
 )
 
 func main() {
@@ -35,7 +36,10 @@ func parseMessages(messages []*sqs.Message) {
 		switch getMessageType(message) {
 		case "CITATION":
 			fmt.Println("Message is a citation")
-			sendTextToPrinter(*message.Body)
+			body := strings.Replace(*message.Body, "!AWOO", "", 1)
+			parts := strings.SplitN(body, "|", 3)
+			name, offence, penalty := parts[0], parts[1], parts[2]
+			createCitation(name, offence, penalty)
 		case "STICKER":
 			fmt.Println("Message is a sticker")
 		case "UNKNOWN":
@@ -63,4 +67,22 @@ func sendTextToPrinter(text string) {
 	}()
 	out, _ := cmd.CombinedOutput()
 	fmt.Printf("%s\n", out)
+}
+
+func sendFileToPrinter(filename string) {
+	cmd := exec.Command("lp", filename)
+	out, _ := cmd.CombinedOutput()
+	fmt.Printf("%s\n", out)
+}
+
+func createCitation(name, offence, penalty string) {
+	fmt.Printf("Name: %v\nCrime: %v\nPenalty: %v\n", name, offence, penalty)
+	form := fillpdf.Form{
+		"Name":    name,
+		"Offence": offence,
+		"Penalty": penalty,
+	}
+	err := fillpdf.Fill(form, "awoo.pdf", "awoo-filled.pdf", true)
+	fmt.Println(err)
+	sendFileToPrinter("awoo-filled.pdf")
 }
