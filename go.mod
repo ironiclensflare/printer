fea294896205e78b12c84da22725385e88a04607
Stuart Carruthers <stu@ironiclensflare.com>
2020-06-25 21:33:05 +0100

    PRIN-7 Added assert module

--- go.mod
+++ go.mod
@@ -6,5 +6,6 @@ require (
 	github.com/aws/aws-sdk-go v1.32.9
 	github.com/cucumber/godog v0.10.0
 	github.com/desertbit/fillpdf v0.0.0-20150721083828-cece5eb91157
+	github.com/stretchr/testify v1.6.1
 	golang.org/x/tools v0.0.0-20200625163451-727c06e3f111 // indirect
 )
--- telegram/sticker_test.go
+++ telegram/sticker_test.go
@@ -1,14 +1,28 @@
 package telegram
 
 import (
+	"errors"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestGetStickerNoId(t *testing.T) {
 	sticker := Sticker{}
 	_, err := sticker.Get("")
-	if err != nil {
-		return
+	expectedError := errors.New("Invalid sticker ID")
+
+	if assert.Error(t, err) {
+		assert.Equal(t, expectedError, err)
 	}
-	t.Error("Did not throw error")
+}
+
+func TestGetStickerValidId(t *testing.T) {
+	t.Skip("Incomplete test")
+
+	sticker := Sticker{}
+	filename, error := sticker.Get("12345")
+
+	assert.Nil(t, error)
+	assert.Equal(t, "test.webp", filename)
 }
